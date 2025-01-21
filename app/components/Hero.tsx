ef07e4c33cfe5959accd6312d15dacfcb25d715a
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-21 17:08:39 +0530

    completed the hero page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -15,22 +18,37 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.29] bg-dot-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.18] bg-dot-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widset text-xs text-center text-blue-100 max-w-80">
+            Dynamic web magic with next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Trasnforming concept into seamless experiences"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m sadiq, a frontend developer and aspiring full-stack
+            engineer based in India.
+          </p>
+          <a href="#about">
+            <MagicButton 
+            title="Show my work"
+              icon ={<FaLocationArrow/>}
+              position="right"
+              
+            />
+          </a>
         </div>
-        <div className="flex justify-center relative my-20 z-10">
-          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <h2 className="uppercase tracking-widset text-xs text-center text-blue-100 max-w-80">Dynamic web magic with next.js</h2>
-
-          </div>
-
-        </div>
-        {/* <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+      </div>
+      {/* <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
         Backgrounds
       </p> */}
-      </div>
-    
+    </div>
   );
 };
 
