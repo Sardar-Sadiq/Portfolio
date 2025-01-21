24d2e6a230120c2beadb8b3dc89315cd7a2a727b
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-21 19:19:26 +0530

    imported the pics of project

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Grid from "./components/Grid";
 // import { FloatingNav } from "./components/ui/FloatingNav";
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
-      <div className="max-2-7xl w-full">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         {/* <FloatingNav/> */}
         <Hero/>
         <Grid/>
