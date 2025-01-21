18fe3225c811c9c3277e8aed720b83e3418f04df
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-22 02:16:39 +0530

    changes in the bento grid

--- app/components/ui/BentoGrid.tsx
+++ app/components/ui/BentoGrid.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import animationData from '@/data/confettie.json';
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+import Image from "next/image";
 export const BentoGrid = ({
   className,
   children,
@@ -72,9 +73,11 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && 'flex justify-center' } h-full`}>
         <div className="w-full h-full absolute px-0">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
+              width={100}
+              height={100}
               className={cn(imgClassName, "object-cover")}
             />
           )}
@@ -85,9 +88,11 @@ export const BentoGridItem = ({
           } `}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
+              width={100}
+              height={100}
               className={"object-cover, object-center w-full h-full"}
             />
           )}
