64f0b2f7545c45d7f7a729c1f4f7d8003fcaa430
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-21 18:48:47 +0530

    created bento grid and added the data file

--- app/components/ui/FloatingNav.tsx
+++ app/components/ui/FloatingNav.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { FloatingNav } from "../ui/floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
-export function FloatingNavDemo() {
+export function FloatingNav() {
   const navItems = [
     {
       name: "Home",
--- app/components/ui/MagicBuuton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const MagicBuuton = () => {
-  return (
-   
-  );
-};
-
-export default MagicBuuton;
