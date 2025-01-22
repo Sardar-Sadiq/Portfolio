78647135db3ecd0ac611c6371ee4c165829971c2
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-22 19:20:36 +0530

    changed the path of components

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import Hero from "./components/Hero";
 import Grid from "./components/Grid";
 import RecentProjects from "./components/RecentProjects";
 
-// import { FloatingNav } from "./components/ui/FloatingNav";
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav /> */}
-        <Hero/>
-        <Grid/>
-        <RecentProjects/>
+        {/* Pass props as per Aceternity UI's requirements */}
+       
+        <Hero />
+        <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
