de7093aa0f4edd104338fb93365fa05dc03b0d81
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-24 01:41:45 +0530

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,9 +23,7 @@ const Hero = () => {
         className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
-        {/* Radial gradient for the container to give a faded look */}
         <div
-          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
@@ -52,9 +50,7 @@ const Hero = () => {
           </Link>
         </div>
       </div>
-      {/* <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Backgrounds
-      </p> */}
+      
     </div>
   );
 };
